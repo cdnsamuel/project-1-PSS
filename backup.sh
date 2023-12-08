2d352f2cdb7f8c062f6228c204b6bd46ed0de98c
Cdnsamuel <cdnsamuel@pm.me>
2023-12-08 12:10:19 +0100

    ok menu

--- backup.sh
+++ backup.sh
@@ -1,11 +1,59 @@
-# Affichage de la liste
-cat folders.list
-# Questionnaire
+#!/bin/bash
 
-# Ajout liste
+running=1
 
-# Supression de la liste
+list=(
+"1) Ajouter une source de sauvegarde"
+"2) Supprimer une source de sauvegarde"
+"3) Modifier la destination de sauvegarde"
+"4) Lancer la sauvegarde "
+"5) Plannifier la sauvegarde"
+"6) Quitter"
+)
 
-# Lancer la sauvegarde
+message()
+{
+	echo
+	printf "%s\n" "${list[@]}"
+    echo
+	read -p "Choisissez une tâche par son numéro: " task
+	echo
+}
+message
 
-## Plannification
+
+while [[ $running = '1' ]]  
+do
+	case $task in
+		1)
+			echo "1" 
+			running=0
+			;;
+		2)
+			echo "2" 
+			running=0
+			;;
+		3) 
+			echo "3" 
+			running=0
+			;;
+		4)
+			echo "4" 
+			running=0
+			;;
+		5)
+			echo "5" 
+			running=0
+			;;
+		6)
+			echo "Quitter" 
+			running=0
+			;;
+		*)
+			echo "Selection invalide"
+			running=1
+			message
+			 
+			;;
+	esac
+done
