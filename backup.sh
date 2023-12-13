d58ec7db6f0342ef90acc9ab1b41dc9cadb5042a
Cdnsamuel <cdnsamuel@pm.me>
2023-12-13 16:10:29 +0100

    done: ajout-supression sources-destination

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ##! FONCTIONS PERSONNALISEES
-
 # vérifier la présence des fichiers, les créer si besoin
 check_files()
 {
@@ -17,25 +16,37 @@ pause(){
 # Afficher la liste des cibles
 show_folder()
 {
-	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "⬇️  Dossiers à sauvegarder"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	if [ -s folder.list ]
 	then
-		echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
-		echo "Dossiers à sauvegarder"
-		echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
 		cat folder.list
-		echo
 	else
-		echo "La liste de dossier à sauvegarder est vide"
+		echo "Aucune source définie"
+	fi
+}
+
+# Option 3:
+show_destination()
+{
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "⬇️  Destination de la sauvegarde"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	if [ -s destination.list ]
+	then
+		cat destination.list
+	else
+		echo "Aucune destination définie"
 	fi
 }
 
 # Afficher le menu 
 show_menu()
 {	
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "Menu Principal"
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "🎛️  Menu Principal"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	echo "1) Ajouter une source de sauvegarde"
 	echo "2) Supprimer une source de sauvegarde"
 	echo "3) Modifier la destination de sauvegarde"
@@ -51,35 +62,100 @@ add_source()
 	echo "Ajout de source sélectionné"
 	pause
 	read -p "Entrez le chemin à ajouter : " new_source
-	read -p "Voulez vous rajouter $new_source à vos dossier à sauvegarder : Y/N " validation
-	case $validation in
-	[Yy]* )
-		echo "$new_source" >> folder.list
-		echo "Le dossier $new_source à bien été ajouté"
+	if [ -d $new_source ]
+	then
+		read -p "Voulez vous rajouter $new_source à vos dossier à sauvegarder : Y/N " validation
+		case $validation in
+		[Yy]* )
+			echo "$new_source" >> folder.list
+			echo "Le dossier $new_source à bien été ajouté"
+			pause
+		;;
+		* )
+			echo "Le dossier $new_source n'a pas été ajouté"
+			pause
+		esac
+	else
+		echo "$new_source n'est pas un dossier"
 		pause
-	;;
-	* )
-		echo "Le dossier $new_source n'a pas été ajouté"
-		pause
-	esac
+	fi
 }
 
 # Supprimer une entrée à folder.list
 del_source()
 {
-	echo "supppression"
+	echo "Suppression de source sélectionné"
+	pause
+	read -p "Entrez le chemin à enlever : " source
+	if grep -q "$source" folder.list
+	then
+		read -p "Voulez vous enlever $source de vos dossier à sauvegarder : Y/N " validation
+		case $validation in
+		[Yy]* )
+			grep -v "$source" folder.list > tmpfile && mv tmpfile folder.list || cp /dev/null folder.list && rm tmpfile
+			if [ $? ] 
+			then
+				echo "Le dossier $source à bien été enlevé"
+				pause
+			else
+				echo "Erreur lors de la supression du dossier $source"
+				pause
+			fi
+		;;
+		* )
+			echo "Annulation, Le dossier $source n'a pas été enlevé"
+			pause
+		esac
+	else
+		echo "$source n'est pas dans la liste des sources de sauvegardes"
+		pause
+	fi
 }
 
 # Editer la destination de sauvegarde
 edit_destination()
 {
-	echo "edition de la destination"
+	echo "Ajout de la destination"
+	pause
+	read -p "Entrez le chemin de la destination : " new_destination
+	if [ -z $new_destination ]
+	then
+		echo "Suppression de la destination de sauvegarde"
+		cp /dev/null destination.list
+		pause
+	else
+		if [ -d $new_destination ]
+		then
+			read -p "Voulez vous que $new_destination devienne votre chemin de sauvegarde : Y/N " validation
+			case $validation in
+			[Yy]* )
+				echo "$new_destination" > destination.list
+				echo "Le dossier $new_destination à bien été ajouté"
+				pause
+			;;
+			* )
+				echo "Le dossier $new_destination n'a pas été ajouté"
+				pause
+			esac
+		else
+			echo "Annulation, $new_source n'est pas un dossier"
+			pause
+		fi
+	fi
 }
 
 # Effectuer la sauvegarde
 launch_backup()
-{
-	echo "début de la sauvegarde"
+{	
+	if [ -s folder.list -a -s destination.list ]
+	then
+		echo "Début de la sauvegarde"
+		pause
+	else
+		if [ ! -s folder.list ]; then echo "Aucune source définie"; fi
+		if [ ! -s destination.list ]; then echo "Aucune destination définie"; fi
+		pause
+	fi
 }
 
 # Changer la plannification
@@ -108,7 +184,9 @@ check_files
 
 while true
 do
+	clear
 	show_folder
+	show_destination
 	show_menu
 	read_option	
 done
